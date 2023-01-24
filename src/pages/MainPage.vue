3906fa560f7dbe6f82d016b5597c991fbce1e7d3
lukajaa <lucaskchang@gmail.com>
2023-01-24 11:12:44 -0800

    popo

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -14,10 +14,11 @@
       <p class="text-h4 text-center justify-center q-ma-lg">
         What is the census?
       </p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Iaculis nunc sed augue lacus. Dolor morbi non arcu risus quis varius quam. Aliquam ultrices sagittis orci a scelerisque purus. Cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Maecenas pharetra convallis posuere morbi leo urna molestie. Et ultrices neque ornare aenean euismod elementum nisi quis. Gravida in fermentum et sollicitudin ac orci. Lacus luctus accumsan tortor posuere ac ut consequat. At consectetur lorem donec massa sapien faucibus et. Tincidunt augue interdum velit euismod. Ipsum faucibus vitae aliquet nec ullamcorper sit amet risus. Diam volutpat commodo sed egestas egestas fringilla phasellus faucibus scelerisque. Scelerisque fermentum dui faucibus in ornare quam viverra. Penatibus et magnis dis parturient montes nascetur ridiculus mus. Tempus urna et pharetra pharetra massa. Feugiat nisl pretium fusce id velit ut tortor pretium viverra. Ut eu sem integer vitae justo eget magna fermentum iaculis.</p>
+      <p class="text-subtitle1">Established in 1790 and administered every decade, the United States Census attempts to survey the entire population and provide the truest measure of all groups through targeted questions and accumulation of a wide variety of data. While data collection and computation initially began with door-to-door efforts, it has evolved to make use of more complex forms of digital surveying and calculations. </p>
       <div class="q-mt-lg shadow-3 q-pa-sm">
-        <p class="text-h6 text-center">Get a random census question!</p>
-
+        <p class="text-h6 text-center">Get a question from any historical census!</p>
+        <p class="text-center text-subtitle1">{{ current_question }}</p>
+        <q-btn color="primary" @click="random_question" label="New Question" />
       </div>
    </div>
     <div class="q-ma-lg q-pa-xl q-mt-none shadow-7 bg-white rounded-borders text-subtitle1 text-center">
@@ -34,7 +35,7 @@
         <template v-slot:after>
           <div class="q-pa-md">
             <div class="text-h4 q-mb-md text-center">Sample</div>
-            <div class="q-my-md">A sample is a subset of the population that is representative, random, and non-biased. </div>
+            <div class="q-my-md">A sample is a subset of the population that is representative, random, and non-biased. For example, one thousand random people from San Francisco.</div>
           </div>
         </template>
       </q-splitter>
@@ -43,13 +44,15 @@
         <q-btn color="white" text-color="black" label="Population" @click="wrong = !wrong; right = false" class="q-mx-sm"/>
         <q-btn color="white" text-color="black" label="Sample" @click="right = !right; wrong = false" class="q-mx-sm"/>
       </div>
-      <div class="q-mt-lg shadow-3" v-if="wrong || right">
-        <p v-if="wrong" class="q-pa-md rounded-borders">You're wrong. The United States Census tries to survey an entire population to “provide the truest measure of all minority groups”, while minimizing sampling errors. However, these ideals fall short. First of all, the census requires a monstrous budget to execute, and it is difficult to administer to all members of the United States. Non-response bias, or a bias that occurs where people who don’t respond to surveys are not counted, accounts for a huge disparity in the enumeration of minority groups and children in particular. For example, the National Academy of Sciences examined data and found a “probable undercount of 1.8% of the general population” (Lerner, Science Clarified).</p>
-        <p v-if="right" class="q-pa-md rounded-borders">You're wrong, but a lot of people think you should be right. The Census can't find the whole population anyway because there will always be people who don't respond, so some people suggest we make it a sample instead. Sampling is both cost-efficient and easier to execute because it uses small sample sizes and statistics to evaluate the size and makeup of a population. Through reliable mathematical formulas based on birth, death, emigration, and immigration records it can count marginalized groups and minorities with accuracy. Also, the errors produced from statistics-based sampling are not based on race, difficulty of acess to certain groups, and are altogether easier to identify, quantify, and rectify.</p>
-      </div>
+      <Transition>
+        <div class="q-mt-lg shadow-3" v-if="wrong || right">
+          <p v-if="wrong" class="q-pa-md rounded-borders">You're wrong. The United States Census tries to survey an entire population to “provide the truest measure of all minority groups”, while minimizing sampling errors. However, these ideals fall short. First of all, the census requires a monstrous budget to execute, and it is difficult to administer to all members of the United States. Non-response bias, or a bias that occurs where people who don’t respond to surveys are not counted, accounts for a huge disparity in the enumeration of minority groups and children in particular. For example, the National Academy of Sciences examined data and found a “probable undercount of 1.8% of the general population” (Lerner, Science Clarified).</p>
+          <p v-if="right" class="q-pa-md rounded-borders">You're wrong, but a lot of people think you should be right. The Census can't find the whole population anyway because there will always be people who don't respond, so some people suggest we make it a sample instead. Sampling is both cost-efficient and easier to execute because it uses small sample sizes and statistics to evaluate the size and makeup of a population. Through reliable mathematical formulas based on birth, death, emigration, and immigration records it can count marginalized groups and minorities with accuracy. Also, the errors produced from statistics-based sampling are not based on race, difficulty of acess to certain groups, and are altogether easier to identify, quantify, and rectify.</p>
+        </div>
+      </Transition>
     </div>
     <div class="q-ma-lg q-pa-xl q-mb-none shadow-7 bg-white rounded-borders">
-      <div class="text-center text-h4">Try it Yourself</div>
+      <div class="text-center text-h4">Try Sampling Yourself!</div>
       <div class="row">
         <div class="col q-mx-sm">
           <q-input v-model="population" label="Population" />
@@ -67,7 +70,7 @@
         </div>
       </div>
       <q-btn color="blue" text-color="white" label="Submit" @click="guess" class="q-ma-sm"/>
-      <p v-if="big">Large populations can take a while to estimate. Be patient and don't spam the button.</p>
+      <Transition>
       <div v-if="answered" class="row text-center text-white">
         <div :class="['col', 'q-ma-sm', 'q-mb-lg', 'rounded-borders', 'bg-' + color]" style="height: 25vh;">
           <h3>Result</h3>
@@ -84,6 +87,8 @@
           </q-tooltip>
         </div>
       </div>
+      </Transition>
+      <Transition>
       <q-list v-if="answered" bordered class="rounded-borders q-ma-lg">
         <q-expansion-item
           expand-separator
@@ -119,10 +124,11 @@
           </q-card>
         </q-expansion-item>
       </q-list>
+      </Transition>
     </div>
     <div class="q-ma-lg q-pa-xl q-mt-none shadow-7 bg-white rounded-borders">
       <h4 class="text-center">Why does this even matter?</h4>
-      <p>Firstly, the Census forms the foundation of congressional districts in the U.S., which influences how votes are apportioned and delegates are selected. If there is an under-assessment of minorities, for example, this poses both the problem of unequal representation and insufficient amounts of government aid (for the clinically disabled, etc.). Finally, a huge fiscal problem would be solved should the Census be reformed: it is estimated that approximately $100 million could be saved, “assuming an overall sampling rate of 75 percent" (Lerner, Science Clarified).</p>
+      <p class="text-subtitle1">Firstly, the Census forms the foundation of congressional districts in the U.S., which influences how votes are apportioned and delegates are selected. If there is an under-assessment of minorities, for example, this poses both the problem of unequal representation and insufficient amounts of government aid (for the clinically disabled, etc.). Finally, a huge fiscal problem would be solved should the Census be reformed: it is estimated that approximately $100 million could be saved, “assuming an overall sampling rate of 75 percent" (Lerner, Science Clarified).</p>
     </div>
     <div class="text-center q-pa-md bg-white shadow-up-3">
       <p class="q-mb-none">Made with Vue3 and Quasar</p>
@@ -137,7 +143,8 @@
           <div class="text-h6">Sources Used</div>
         </q-card-section>
         <q-card-section class="q-pt-none">
-          - Nagel, M C. “Should Statistical Sampling Be Used in the United States Census?” Science Clarified, Science Clarified, www.scienceclarified.com/dispute/Vol-2/Should-statistical-sampling-be-used-in-the-United-States-Census.html.
+          <p>- Nagel, M C. “Should Statistical Sampling Be Used in the United States Census?” Science Clarified, Science Clarified, www.scienceclarified.com/dispute/Vol-2/Should-statistical-sampling-be-used-in-the-United-States-Census.html.</p>
+          <p>- “Lincoln Index.” Wikipedia, Wikimedia Foundation, 11 Aug. 2021, en.wikipedia.org/wiki/Lincoln_index.</p>
         </q-card-section>
       </q-card>
     </q-dialog>
@@ -149,11 +156,12 @@ import { ref, computed, watch } from 'vue';
 
 const population = ref(5024279);
 const sourcesModal = ref(false);
-const big = ref(false);
 const state = ref('Alabama');
 const options = ref({'California':39538223,'Texas':29145505,'Florida':21538187,'New York':20201249,'Pennsylvania':13002700,'Illinois':12812508,'Ohio':11799448,'Georgia':10711908,'North Carolina':10439388,'Michigan':10077331,'New Jersey':9288994,'Virginia':8631393,'Washington':7705281,'Arizona':7151502,'Massachusetts':7029917,'Tennessee':6910840,'Indiana':6785528,'Maryland':6177224,'Missouri':6154913,'Wisconsin':5893718,'Colorado':5773714,'Minnesota':5706494,'South Carolina':5118425,'Alabama':5024279,'Louisiana':4657757,'Kentucky':4505836,'Oregon':4237256,'Oklahoma':3959353,'Connecticut':3605944,'Utah':3271616,'Iowa':3190369,'Nevada':3104614,'Arkansas':3011524,'Mississippi':2961279,'Kansas':2937880,'New Mexico':2117522,'Nebraska':1961504,'Idaho':1839106,'West Virginia':1793716,'Hawaii':1455271,'New Hampshire':1377529,'Maine':1362359,'Rhode Island':1097379,'Montana':1084225,'Delaware':989948,'South Dakota':886667,'North Dakota':779094,'Alaska':733391,'Vermont':643077,'Wyoming':576851});
+const questions = ref(['How many people are living or staying in this house, apartment, or mobile home?', 'Does Person 1 sometimes live or stay somewhere else?', 'What state or country was this person from?', 'Has this person ever served on active duty in the U.S. Armed Forces, military Reserves, or National Guard?', 'What kind of work was this person doing?', 'About when was this building first built?', 'Do you have complete plumbing facilities in this house, apartment or mobile home?', 'Do you have complete kitchen facilities in this house, apartment or mobile home?', 'Is this building connected to the public sewer?', 'Do you have complete plumbing facilities in your living quarters, that is, hot and cold piped water, a flush toilet, and a bathtub or shower?', 'Do you have air conditioning?', 'How many passenger automobiles are owned or regularly used by members of your household?', 'Do you have a flush toilet?', 'Do you have a bathtub or shower?', 'Is there a basement in this building?', 'Do you have a battery-operated radio?', 'Do you have a television set?', 'Does the household have an electric or gas clothes dryer?', 'Does the household have a home food freezer which is separate from its refrigerator?', 'What city and county did this person work in last week?', 'Corresponding agriculture questionnaire number', 'Is this house on a farm (or ranch)? (Yes or No)', 'Is the person a survivor of the Union or Confederate Army or Navy?', 'Can the person not read?', 'Can the person not write?']);
+const current_question = ref('How many people are in your household?')
 const sample_one = ref(2512140);
-const sample_two = ref(25000);
+const sample_two = ref(1256070);
 const result = ref(0);
 const difference = ref(0);
 const percent_difference = ref(0);
@@ -178,6 +186,10 @@ function onScroll (position) {
   } else {
     restart.value = false
   }
+}
+
+function random_question() {
+  current_question.value = questions.value[Math.floor(Math.random() * questions.value.length)]
 }
 
 watch(state, (state_name : string) => {
@@ -205,9 +217,10 @@ function reset() {
     behavior: 'smooth'
   });
   answered.value = false;
-  population.value = 100000;
-  sample_one.value = 75000;
-  sample_two.value = 75000;
+  population.value = 5024279;
+  sample_one.value = 2512140;
+  sample_two.value = 1256070;
+  state.value = 'Alabama'
   wrong.value = false
   right.value = false
 }
